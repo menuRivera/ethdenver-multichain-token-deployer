e98cd4f27b379b98a6ca61eff568d1399b122a09
Manuel Rivera <menu.rivera@gmail.com>
2024-02-29 13:47:45 -0600

    [DEL|MOD] -> contract renaming

--- nft-contract/src/NFT.sol
+++ nft-contract/src/NFT.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+// change this to NFT 721
+contract NFT {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
