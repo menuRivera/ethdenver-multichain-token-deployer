3b4fe2539996a699b3b7059f1d37485f49acaf89
Manuel Rivera <menu.rivera@gmail.com>
2024-03-02 05:49:45 -0600

    [REDO] -> frontend

--- canisters/src/frontend/src/App.tsx
+++ canisters/src/frontend/src/App.tsx
@@ -1,179 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import { useState } from 'react';
+import { Box, Button, Card, Image, Input, Label } from 'theme-ui';
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: '',
-    symbol: '',
-    description: '',
-    image: '',
-    animation_url: '',
-    external_url: '',
-    attributes: [],
-  });
 
-  useEffect(() => {
-    // Fetch data when the component mounts
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.example.com/data'); // Replace with your actual API endpoint
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
+	const [name, setName] = useState('')
+	const [symbol, setSymbol] = useState('')
+	const [description, setDescription] = useState('')
+	const [image, setImage] = useState('')
 
-        const data = await response.json();
-        // Update the state with the fetched data
-        setFormData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault()
 
-    fetchData();
-  }, []); // The empty dependency array ensures this effect runs only once on mount
+		const body = { name, symbol, description, image }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+		alert(JSON.stringify(body))
 
-  const handleAttributeChange = (index, field, value) => {
-    setFormData((prevData) => {
-      const updatedAttributes = [...prevData.attributes];
-      updatedAttributes[index] = {
-        ...updatedAttributes[index],
-        [field]: value,
-      };
-      return {
-        ...prevData,
-        attributes: updatedAttributes,
-      };
-    });
-  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Add logic to handle form submission, e.g., send data to server
-    // fetch(url, {method, body})
-    console.log('Form submitted:', formData);
-  };
+		// todo: add fetch here
+		// const res = await (await fetch('our url', {
+		// 	method: 'POST',
+		// 	body: JSON.stringify(fetch),
+		// 	headers: { 'Content-Type': 'application/json' }
+		// })).json()
+	};
 
-  return (
-    <div className="container">
-      <h1>NFT Multichain Deployer</h1>
-      <div className="card">
-        <form onSubmit={handleSubmit}>
-          {/* Basic Information */}
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </div>
+	return (
+		<div className="container">
+			<Card>
+				<Box as="form" onSubmit={handleSubmit}>
+					<Image sx={{ width: '50%', margin: 'auto' }} src='/Token_Magician.png'></Image>
 
-          <div className="form-group">
-            <label htmlFor="symbol">Symbol:</label>
-            <input
-              type="text"
-              id="symbol"
-              name="symbol"
-              value={formData.symbol}
-              onChange={handleChange}
-            />
-          </div>
+					<Label>
+						Name
+						<Input onChange={(e) => setName(e.target.value)} />
+					</Label>
+					<Label>
+						Symbol
+						<Input onChange={(e) => setSymbol(e.target.value)} />
+					</Label>
+					<Label>
+						Description
+						<Input onChange={(e) => setDescription(e.target.value)} />
+					</Label>
+					<Label>
+						Image url
+						<Input onChange={(e) => setImage(e.target.value)} />
+					</Label>
 
-          <div className="form-group">
-            <label htmlFor="description">Description:</label>
-            <textarea
-              id="description"
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-            ></textarea>
-          </div>
 
-          <div className="form-group">
-            <label htmlFor="image">Image URL:</label>
-            <input
-              type="text"
-              id="image"
-              name="image"
-              value={formData.image}
-              onChange={handleChange}
-            />
-          </div>
-
-          {/* Additional Information */}
-          <div className="form-group">
-            <label htmlFor="animation_url">Animation URL:</label>
-            <input
-              type="text"
-              id="animation_url"
-              name="animation_url"
-              value={formData.animation_url}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="external_url">External URL:</label>
-            <input
-              type="text"
-              id="external_url"
-              name="external_url"
-              value={formData.external_url}
-              onChange={handleChange}
-            />
-          </div>
-
-          {/* Attributes */}
-          <div className="form-group">
-            <label>Attributes:</label>
-            {formData.attributes.map((attr, index) => (
-              <div key={index} className="attribute-group">
-                <input
-                  type="text"
-                  placeholder="Trait Type"
-                  value={attr.trait_type || ''}
-                  onChange={(e) =>
-                    handleAttributeChange(index, 'trait_type', e.target.value)
-                  }
-                />
-                <input
-                  type="text"
-                  placeholder="Value"
-                  value={attr.value || ''}
-                  onChange={(e) =>
-                    handleAttributeChange(index, 'value', e.target.value)
-                  }
-                />
-              </div>
-            ))}
-            <button
-              type="button"
-              onClick={() =>
-                setFormData((prevData) => ({
-                  ...prevData,
-                  attributes: [...prevData.attributes, { trait_type: '', value: '' }],
-                }))
-              }
-            >
-              Add Attribute
-            </button>
-          </div>
-
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    </div>
-  );
+					<Button sx={{ backgroundColor: 'darkblue' }} type='submit' variant='dark'>Submit</Button>
+				</Box>
+			</Card>
+		</div>
+	);
 }
 
 export default App;
--- canisters/src/frontend/src/main.tsx
+++ canisters/src/frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Theme, ThemeUIProvider } from 'theme-ui'
 import './index.css'
 
+const theme: Theme = {
+	cards: {
+		primary: {
+			backgroundColor: '#ece5e5',
+			borderRadius: '10px',
+			margin: '10vh 10vw',
+			padding: '100px',
+			boxShadow: '5px 5px 20px 10px lightGray'
+		}
+	},
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<ThemeUIProvider theme={theme}>
+			<App />
+		</ThemeUIProvider>
+	</React.StrictMode>,
 )
